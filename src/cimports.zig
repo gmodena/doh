ed5384add2b0b6c0206cf9d07d4a862aad4d2a67
Gabriele Modena <gmodena@pm.me>
2026-07-10 22:10:35 +0200

    wip: disable macros/pragrmas to build with ReleaseSafe.
    Why:
    
    We want to build doh with ReleaseSafe.
    
    What:
    
    Disable empty aggregate support to avoid gcc pragma
    directives that zig's c translator (possibly)
    cannot handle.
    
    Disable glibc fortification with -O2 (ReleaseSafe).
    
    Signed-off-by: Gabriele Modena <gmodena@pm.me>

--- src/cimports.zig
+++ src/cimports.zig
@@ -1,9 +1,12 @@
 // Shared C imports to ensure consistent opaque types across modules
 pub const c = @cImport({
     @cDefine("XSTAT_TYPE", "struct stat");
-    // Disable empty aggregate support to avoid GCC pragma directives that
-    // Zig's C translator cannot handle (WOLF_AGG_DUMMY_MEMBER in hash.h).
+    // WIP: Disable empty aggregate support to avoid gcc pragma
+    // directives that zig's c translator cannot handle
+    // (WOLF_AGG_DUMMY_MEMBER in hash.h).
     @cDefine("HAVE_EMPTY_AGGREGATES", "0");
+    // WIP: Disable glibc fortification: with -O2 (ReleaseSafe).
+    @cUndef("_FORTIFY_SOURCE");
     @cInclude("wolfssl/options.h");
     @cInclude("wolfssl/wolfcrypt/settings.h");
     @cInclude("wolfssl/ssl.h");
